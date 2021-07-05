1c40fbbb245bc10cab5d073414e6d6960f0e52c7
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-07-05 16:07:54 +0200

    add title

---
+++ vue-cli-first/src/components/Title.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+    <h1>la mia prima app con Vue CLI!</h1>
+</template>
+
+
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+    h1 {
+        font-size: 50px;
+        text-transform: capitalize;
+    }
+</style>
